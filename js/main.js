8109b8c2d5e3605b55017f9ae3d8a20daa5e37b6
Tatiana <tatianasstavares@gmail.com>
2021-12-29 22:38:10 +0000

    Add contact page.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var toastTrigger = document.getElementById('liveToastBtn')
+
+var toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+
+  toastTrigger.addEventListener('click', function () {
+
+    var toast = new bootstrap.Toast(toastLiveExample)
+    
+    toast.show()
+
+  })
+
+}
